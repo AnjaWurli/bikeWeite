789a664f7d3f6afe7c44ec23b1da6b2de480ffc8
AnjaWurli <anja.wurlitzer.coding@gmail.com>
2023-07-11 12:21:40 +0200

    add landing page

---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>bike Weite</h1>
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  justify-content: space-around;
+}
+nav {
+  font-size: 12px;
+  text-align: center;
+  margin-top: 1rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  .wrapper {
+    flex-direction: column;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
